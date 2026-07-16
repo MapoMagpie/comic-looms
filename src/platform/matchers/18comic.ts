3249ecf46a94e70e4761f834e1da946a18ccf76b
MapoMagpie <zsyjk@live.cn>
2026-07-16 18:48:55 +0800

    support: comic18j-codi.net #233;

--- src/platform/matchers/18comic.ts
+++ src/platform/matchers/18comic.ts
@@ -168,6 +168,7 @@ ADAPTER.addSetup({
   workURLs: [
     /(18|jm)comic.*?\/album\/\d+/,
     /jm18c-.*?\/album\/\d+/,
+    /comic18j-codi.net\/album\/\d+/,
   ],
   match: ["https://18comic.vip/*"],
   constructor: () => new Comic18Matcher(),
