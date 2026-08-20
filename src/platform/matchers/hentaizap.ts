34a79762e2d946992d4637b756acc1ff3bcd579e
MapoMagpie <zsyjk@live.cn>
2026-08-20 21:21:24 +0800

    fix: hentaizap.com;

--- src/platform/matchers/hentaizap.ts
+++ src/platform/matchers/hentaizap.ts
@@ -3,75 +3,107 @@ import ImageNode from "../../img-node";
 import { ADAPTER } from "../adapt";
 import { BaseMatcher, OriginMeta, Result } from "../platform";
 
-type HentaiZapGalleryInfo = {
-  serverID: string,
-  galleryID: string,
-  loadDir: string,
-  loadID: string,
-  loadPages: number,
-  images: Record<string, string>,
+type HentaiZapGalleryImage = {
+  thumb: string,
+  href: string,
+  page: number,
+  ext: string,
+  width: number,
+  height: number,
+}
+type HentaiZapGalleryPageInfo = {
+  page: number,
+  ext: string,
+  width: number,
+  height: number,
+}
+type HentaiZapGalleryThumbnail = {
+  page: number,
+  alt: string,
+  href: string,
+  thumb: string,
+}
+type HentaiZapGalleryThumbs = {
+  ok: boolean,
+  gallery_id: number,
+  from: number,
+  count: number,
+  thumbs: HentaiZapGalleryThumbnail[],
+  thumb_template: string,
+  pages_total: number,
 }
 
-const HENTAIZAP_TYPE_MAP: Record<string, string> = {
-  "j": "jpg",
-  "p": "png",
-  "b": "bmp",
-  "g": "gif",
-  "w": "webp",
-};
-
-class HentaiZapMatcher extends BaseMatcher<HentaiZapGalleryInfo> {
+class HentaiZapMatcher extends BaseMatcher<HentaiZapGalleryImage[]> {
   meta?: GalleryMeta;
 
   galleryMeta(): GalleryMeta {
     if (this.meta) return this.meta;
-    const title = document.querySelector(".gp_top_right > h1")?.textContent ?? document.title;
+    const title = document.querySelector("#gallery-title")?.textContent ?? document.title;
+    const originTitle = document.querySelector("p.hz-gallery-description")?.textContent;
     this.meta = new GalleryMeta(window.location.href, title);
-    Array.from(document.querySelectorAll<HTMLElement>(".gp_top_right_info > ul")).forEach(ul => {
-      const category = ul.querySelector("span.info_txt")?.textContent?.replace(":", "")?.toLowerCase();
+    this.meta.originTitle = originTitle;
+    Array.from(document.querySelectorAll<HTMLElement>(".hz-gallery-metadata > div.hz-gallery-entity-group")).forEach(elem => {
+      const category = elem.querySelector("span.hz-gallery-entity-label")?.textContent?.replace(":", "")?.toLowerCase();
       if (!category) return;
-      const tags = Array.from(ul.querySelectorAll<HTMLElement>("a.gp_btn_tag")).map(e => e.firstChild?.textContent).filter(Boolean) as string[];
+      const tags = Array.from(elem.querySelectorAll<HTMLElement>("a.hz-gallery-tag > span.hz-gallery-tag__name")).map(e => e.textContent).filter(Boolean) as string[];
       this.meta!.tags[category] = tags;
     });
     return this.meta;
   }
 
-  async *fetchPagesSource(): AsyncGenerator<Result<HentaiZapGalleryInfo>> {
-    const gthRaw = Array.from(document.querySelectorAll<HTMLScriptElement>("script"))
-      .find(e => e.textContent?.trimStart()?.startsWith("var g_th"))
-      ?.textContent?.match(/\('(\{.*\})'\)/)?.[1];
-    if (!gthRaw) throw new Error("cannot find g_th");
-    const serverID = document.querySelector<HTMLInputElement>("input#load_server")?.value;
-    if (!serverID) throw new Error("cannot find server id");
-    const loadDir = document.querySelector<HTMLInputElement>("input#load_dir")?.value;
-    if (!loadDir) throw new Error("cannot find load dir");
-    const galleryID = document.querySelector<HTMLInputElement>("input#gallery_id")?.value;
-    if (!galleryID) throw new Error("cannot find gallery id");
-    const loadID = document.querySelector<HTMLInputElement>("input#load_id")?.value;
-    if (!loadID) throw new Error("cannot find load id");
-    const loadPages = document.querySelector<HTMLInputElement>("input#load_pages")?.value;
-    if (!loadPages) throw new Error("cannot find load pages");
-    const gth = JSON.parse(gthRaw) as Record<string, string>;
-    const info: HentaiZapGalleryInfo = {
-      serverID, galleryID, loadDir, loadID, loadPages: parseInt(loadPages), images: gth
-    };
-    yield Result.ok(info);
+  async *fetchPagesSource(): AsyncGenerator<Result<HentaiZapGalleryImage[]>> {
+    // https://hentaizap.com/gallery/1626345/
+    const gid = window.location.href.match(/gallery\/(\d+)/)?.[1];
+    if (!gid) throw new Error("cannot match gallery id from href: " + window.location.href);
+    // fetch thumbs api
+    const qs = new URLSearchParams();
+    qs.set("from", "1");
+    qs.set("all", "1");
+    const apiUrl = `${window.location.origin}/api/gallery/${gid}/thumbs?${qs.toString()}`;
+    const data = await window.fetch(apiUrl).then(resp => resp.json()).then(data => data as HentaiZapGalleryThumbs).catch(Error);
+    if (data instanceof Error) throw data;
+    if (!data.ok || data.thumbs.length === 0) throw new Error("fetch thumbs api error, response not ok");
+    let firstHref = data.thumbs[0].href;
+    if (!firstHref.startsWith("http")) {
+      firstHref = window.location.origin + firstHref;
+    }
+    // find first picture href, query document, find pageInfo from script
+    const firstDoc = await window.fetch(firstHref).then(resp => resp.text()).catch(Error);
+    if (firstDoc instanceof Error) throw firstDoc;
+    const pageInfoRaw = firstDoc.match(/id="readerPagesJson"\>(\[.*?\])/)?.[1];
+    if (!pageInfoRaw) throw new Error("cannot match pageInfo from " + firstHref);
+    const pageInfos = JSON.parse(pageInfoRaw) as HentaiZapGalleryPageInfo[];
+    if (pageInfos.length !== data.thumbs.length) throw new Error("thumbs length not equals page infos length");
+    const ret: HentaiZapGalleryImage[] = [];
+    for (let i = 0; i < pageInfos.length; i++) {
+      const info = pageInfos[i];
+      const thumb = data.thumbs[i];
+      ret.push({
+        thumb: thumb.thumb,
+        href: thumb.href,
+        page: thumb.page,
+        ext: info.ext,
+        width: info.width,
+        height: info.height
+      });
+    }
+    yield Result.ok(ret);
   }
 
-  async parseImgNodes(info: HentaiZapGalleryInfo): Promise<ImageNode[]> {
-    const server = `m${info.serverID}.hentaizap.com`;
+  async parseImgNodes(images: HentaiZapGalleryImage[]): Promise<ImageNode[]> {
     const nodes: ImageNode[] = [];
-    const digits = info.loadPages.toString().length;
-    for (let i = 0; i < info.loadPages; i++) {
-      // https://m10.hentaizap.com/029/ebsm4v8rz0/1t.jpg
-      const [t, w, h] = info.images[(i + 1).toString()]?.split(",") ?? [];
-      if (!t || !w || !h) throw new Error("cannot find image g_th: " + (i + 1));
-      const ext = HENTAIZAP_TYPE_MAP[t] ?? "webp";
-      const thumb = `https://${server}/${info.loadDir}/${info.loadID}/${i + 1}t.jpg`;
-      const href = `${window.location.origin}/g/${info.galleryID}/${i + 1}/`;
-      const origin = `https://${server}/${info.loadDir}/${info.loadID}/${i + 1}.${ext}`;
-      const title = (i + 1).toString().padStart(digits, "0");
-      const node = new ImageNode(thumb, href, `${title}.${ext}`, undefined, origin, { w: parseInt(w), h: parseInt(h) });
+    const digits = images.length.toString().length;
+    for (let i = 0; i < images.length; i++) {
+      const img = images[i];
+      const title = img.page.toString().padStart(digits, "0");
+      const thumb = img.thumb;
+      const idx = thumb.lastIndexOf("/");
+      const origin = `${thumb.substring(0, idx)}/${img.page}.${img.ext ?? "webp"}`;
+      let href = img.href;
+      if (!href.startsWith("http")) {
+        href = window.location.origin + href;
+      }
+      const node = new ImageNode(thumb, href, `${title}.${img.ext}`, undefined, origin, { w: img.width, h: img.height });
       nodes.push(node);
     }
     return nodes;
